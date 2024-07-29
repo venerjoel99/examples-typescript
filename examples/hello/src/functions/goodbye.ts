4b8de0b8f8bb40ae39ebff0a463b0ea3fd352cb7
aboutphilippe <philippemarketing@gmail.com>
2024-07-29 20:51:02 +0200

    intel

--- examples/hello/src/functions/goodbye.ts
+++ examples/hello/src/functions/goodbye.ts
@@ -6,7 +6,6 @@ interface Output {
   message: string;
 }
 
-// Original activity function
 export async function goodbye(input: Input): Promise<Output> {
   return { message: `Goodbye, ${input.name}!` };
 }
