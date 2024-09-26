f5b9f2df940937443dd03c655dbb1156b969df09
aboutphilippe <philippemarketing@gmail.com>
2024-09-26 16:06:08 +0200

    envs

--- examples/nextjs/src/app/actions/trigger.ts
+++ examples/nextjs/src/app/actions/trigger.ts
@@ -3,13 +3,13 @@ import Restack from "@restackio/restack-sdk-ts";
 import { Example } from "../components/examplesList";
 
 const connectionOptions = {
-  envId: process.env.RESTACK_ENGINE_ENV_ID!,
-  address: process.env.RESTACK_ENGINE_ENV_ADDRESS!,
-  apiKey: process.env.RESTACK_ENGINE_ENV_API_KEY!,
+  envId: process.env.RESTACK_ENGINE_ID!,
+  address: process.env.RESTACK_ENGINE_ADDRESS!,
+  apiKey: process.env.RESTACK_ENGINE_API_KEY!,
 };
 
 const client = new Restack(
-  process.env.RESTACK_ENGINE_ENV_API_KEY ? connectionOptions : undefined
+  process.env.RESTACK_ENGINE_API_KEY ? connectionOptions : undefined
 );
 
 export async function triggerWorkflow(
--- examples/voice/src/client.ts
+++ examples/voice/src/client.ts
@@ -3,11 +3,11 @@ import Restack from "@restackio/restack-sdk-ts";
 import "dotenv/config";
 
 export const connectionOptions = {
-  envId: process.env.RESTACK_ENGINE_ENV_ID!,
-  address: process.env.RESTACK_ENGINE_ENV_ADDRESS!,
-  apiKey: process.env.RESTACK_ENGINE_ENV_API_KEY!,
+  envId: process.env.RESTACK_ENGINE_ID!,
+  address: process.env.RESTACK_ENGINE_ADDRESS!,
+  apiKey: process.env.RESTACK_ENGINE_API_KEY!,
 };
 
 export const client = new Restack(
-  process.env.RESTACK_ENGINE_ENV_API_KEY ? connectionOptions : undefined
+  process.env.RESTACK_ENGINE_API_KEY ? connectionOptions : undefined
 );
