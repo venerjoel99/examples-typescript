26996ef07c52529e97869065b83f1563c5a2dee3
aboutphilippe <philippemarketing@gmail.com>
2024-09-27 10:11:54 +0200

    consistent envs

--- examples/hello/restack_up.mjs
+++ examples/hello/restack_up.mjs
@@ -1,20 +1,21 @@
 import { RestackCloud } from "@restackio/restack-sdk-cloud-ts";
+import "dotenv/config";
 
 const main = async () => {
   const restackCloudClient = new RestackCloud(process.env.RESTACK_CLOUD_TOKEN);
 
   const restackEngineEnvs = [
     {
-      name: "RESTACK_ENGINE_ENV_ID",
-      value: process.env.RESTACK_ENGINE_ENV_ID,
+      name: "RESTACK_ENGINE_ID",
+      value: process.env.RESTACK_ENGINE_ID,
     },
     {
-      name: "RESTACK_ENGINE_ENV_ADDRESS",
-      value: process.env.RESTACK_ENGINE_ENV_ADDRESS,
+      name: "RESTACK_ENGINE_ADDRESS",
+      value: process.env.RESTACK_ENGINE_ADDRESS,
     },
     {
-      name: "RESTACK_ENGINE_ENV_API_KEY",
-      value: process.env.RESTACK_ENGINE_ENV_API_KEY,
+      name: "RESTACK_ENGINE_API_KEY",
+      value: process.env.RESTACK_ENGINE_API_KEY,
     },
   ];
 
