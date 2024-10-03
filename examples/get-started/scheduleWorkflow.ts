560b63affeb5daf1ecfffe99ba2f7659a27ae978
Andres Tapia <andres@restack.io>
2024-10-03 15:49:11 +0200

    Update scheduleWorkflow.ts

--- examples/get-started/scheduleWorkflow.ts
+++ examples/get-started/scheduleWorkflow.ts
@@ -2,7 +2,7 @@ import { client } from "./src/client";
 
 async function scheduleWorkflow() {
 
-    const workflowId = `${Date.now()}-helloWorkflow`;
+    const workflowId = `${Date.now()}-greetingWorkflow`;
     const runId = await client.scheduleWorkflow({
         workflowName: "greetingWorkflow",
         workflowId,
