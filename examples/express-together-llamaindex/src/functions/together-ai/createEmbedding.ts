990d72a2304acf549b8cd9531de3dd6189193cb0
martinibach <martin@restack.io>
2024-11-07 18:03:47 +0100

    simplify and remove createEmbedding function for now

--- examples/express-together-llamaindex/src/functions/together-ai/createEmbedding.ts
+++
@@ -1,7 +0,0 @@
-import Together from 'together-ai';
-import { togetherClient } from './utils/client';
-
-export async function togetherCreateEmbedding(params: Together.Embeddings.EmbeddingCreateParams) {
-    const response = await togetherClient.embeddings.create(params);
-    return response;
-}
--- examples/express-together-llamaindex/src/functions/together-ai/index.ts
+++ examples/express-together-llamaindex/src/functions/together-ai/index.ts
@@ -1,2 +1,1 @@
 export * from './chatCompletionBasic';
-export * from './createEmbedding';
